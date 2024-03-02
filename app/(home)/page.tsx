53058eb196082e5923169ec529419a6d778f29cb
Rahul577503 <rahulmaurya109626@gmail.com>
2024-03-02 21:27:00 +0530

    Update page.tsx

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -8,10 +8,10 @@ const Page = () => {
   return (
     <div className="py-10">
       <div className="dark:bg-black bg-black dark:bg-grid-white/[0.05] bg-grid-white/[0.03] relative">
-        <div className=" mx-auto p-5">
+        <div className="p-5">
           <HeroSection />
         </div>
-        <div className="h-20 xl:h-32 bg-gradient-to-t from-black absolute -bottom-5 left-0 xl-bottom-0 w-full"></div>
+        <div className="h-20 xl:h-32 bg-gradient-to-t from-black absolute -bottom-5 left-0 xl-bottom-0 min-w-full"></div>
         <div className="mx-auto p-5 text-white mt-20">
           <Skills />
           <Project />
